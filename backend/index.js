2d7e8d76820232d29d80aacf1c49a70c4946ea37
ViniciusL <vinilucasreis1999@yahoo.com.br>
2020-03-24 11:27:01 -0300

    Nodemon adicionado como dependência de dev

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+//informa que o corpo das requisições devem ser parseados para JSON
+app.use(express.json());
+
 app.get('/', (request, response) => {
 	return response.json({ message: 'Hello World' });
 });
