95fdb52f3af002a08084f9d1d359c970eb322b10
ViniciusL <vinilucasreis1999@yahoo.com.br>
2020-03-25 12:10:12 -0300

    Página de Login finalizada

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './global.css';
 
-function App() {
-	return <div className='App'></div>;
+import Logon from './pages/Logon';
+
+export default function App() {
+  const [counter, setCounter] = useState(0);
+  const [left_counter, setLeftCounter] = useState(10);
+
+  const incCounter = () => setCounter(counter + 1);
+  const decLeftCounter = () => setLeftCounter(left_counter - 1);
+
+  return (
+    <>
+      <Logon />
+    </>
+  );
 }
-
-export default App;
