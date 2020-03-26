f9370ae93aff88f2af6227e4f2db729e86ac54e9
Vinícius Reis <vinicius.reis@aevo.com.br>
2020-03-26 10:20:31 -0300

    Funcionalidade de Login adicionada

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,33 +1,57 @@
-import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { FiLogIn } from "react-icons/fi";
 
-import './styles.css';
-import logoImg from '../../assets/logo.svg';
-import heroesImg from '../../assets/heroes.png';
-import LinkWrapper from '../../wrappers/LinkWrapper';
+import "./styles.css";
+import logoImg from "../../assets/logo.svg";
+import heroesImg from "../../assets/heroes.png";
+import LinkWrapper from "../../wrappers/LinkWrapper";
+import api from "../../services/api";
 
 export default function Logon() {
-	return (
-		<div className='logon-container'>
-			<section className='form'>
-				<img src={logoImg} alt='Be The Hero' />
+  const history = useHistory();
+  const [id, setId] = useState("");
 
-				<form>
-					<h1>Faça seu logon</h1>
+  const handleLogon = async event => {
+    event.preventDefault();
 
-					<input placeholder='Sua ID' />
-					<button type='submit' className='button'>
-						Entrar
-					</button>
+    await api
+      .post("sessions", { id })
+      .then(response => {
+        localStorage.setItem("ongId", id);
+        localStorage.setItem("ongName", response.data.name);
 
-					<LinkWrapper className='text-link' to='/register'>
-						<FiLogIn size={16} color='#E02041' />
-						Não tenho cadastro
-					</LinkWrapper>
-				</form>
-			</section>
+        history.push("/profile");
+      })
+      .catch(reason => alert("Falha no login: Id não encontrado."));
+  };
 
-			<img src={heroesImg} alt='Heroes' />
-		</div>
-	);
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be The Hero" />
+
+        <form onSubmit={handleLogon}>
+          <h1>Faça seu logon</h1>
+
+          <input
+            placeholder="Sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
+
+          <button type="submit" className="button">
+            Entrar
+          </button>
+
+          <LinkWrapper className="text-link" to="/register">
+            <FiLogIn size={16} color="#E02041" />
+            Não tenho cadastro
+          </LinkWrapper>
+        </form>
+      </section>
+
+      <img src={heroesImg} alt="Heroes" />
+    </div>
+  );
 }
