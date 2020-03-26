58ee776bb9d11592d592f746b84b912f77dabcfb
Vinícius Reis <vinicius.reis@aevo.com.br>
2020-03-26 10:10:49 -0300

    Funcionalidade de Registro de ONGs adicionado

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,44 +1,90 @@
-import React from 'react';
-import './styles.css';
+import React, { useState } from "react";
+import { FiArrowLeft } from "react-icons/fi";
+import "./styles.css";
 
-import logoImg from '../../assets/logo.svg';
-import LinkWrapper from '../../wrappers/LinkWrapper';
-import { FiArrowLeft } from 'react-icons/fi';
+import logoImg from "../../assets/logo.svg";
+import LinkWrapper from "../../wrappers/LinkWrapper";
+import api from "../../services/api";
+import { useHistory } from "react-router-dom";
 
 export default function Register() {
-	return (
-		<div className='register-container'>
-			<div className='content'>
-				<section>
-					<img src={logoImg} alt='Be The Hero' />
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [whatsapp, setWhatsapp] = useState("");
+  const [city, setCity] = useState("");
+  const [uf, setUf] = useState("");
 
-					<h1>Cadastro</h1>
-					<p>
-						Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem
-						os casos da sua ONG.
-					</p>
+  const history = useHistory();
 
-					<LinkWrapper className='text-link' to='/'>
-						<FiArrowLeft size={16} color='#E02041' />
-						Não tenho cadastro
-					</LinkWrapper>
-				</section>
+  const handleRegister = async event => {
+    event.preventDefault();
 
-				<form>
-					<input placeholder='Nome da ONG' />
-					<input type='email' placeholder='E-mail' />
-					<input placeholder='WhatsApp' />
+    await api
+      .post("ongs", { name, email, whatsapp, city, uf })
+      .then(response => {
+        alert(`Seu ID de acesso: ${response.data.id}`);
+        history.push("/"); //Navega para a rota informada, acrescentando-a à pilha de páginas
+      })
+      .catch(reason => alert("Erro no cadastro, tente novamente."));
+  };
 
-					<div className='input-group'>
-						<input placeholder='Cidade' />
-						<input placeholder='UF' style={{ width: 80 }} />
-					</div>
+  return (
+    <div className="register-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="Be The Hero" />
 
-					<button type='submit' className='button'>
-						Cadastrar
-					</button>
-				</form>
-			</div>
-		</div>
-	);
+          <h1>Cadastro</h1>
+          <p>
+            Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem
+            os casos da sua ONG.
+          </p>
+
+          <LinkWrapper className="text-link" to="/">
+            <FiArrowLeft size={16} color="#E02041" />
+            Não tenho cadastro
+          </LinkWrapper>
+        </section>
+
+        <form onSubmit={handleRegister}>
+          <input
+            placeholder="Nome da ONG"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+
+          <input
+            type="email"
+            placeholder="E-mail"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+
+          <input
+            placeholder="WhatsApp"
+            value={whatsapp}
+            onChange={e => setWhatsapp(e.target.value)}
+          />
+
+          <div className="input-group">
+            <input
+              placeholder="Cidade"
+              value={city}
+              onChange={e => setCity(e.target.value)}
+            />
+            <input
+              placeholder="UF"
+              style={{ width: 80 }}
+              value={uf}
+              onChange={e => setUf(e.target.value)}
+            />
+          </div>
+
+          <button type="submit" className="button">
+            Cadastrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
