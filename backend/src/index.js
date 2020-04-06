7a5adf9c39fcd4503339e7db76fed8111cf6e434
VLRTroll <viniciusldr99@gmail.com>
2020-04-06 10:48:22 -0300

    Validações iniciais de requisições inseridas no backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const routes = require('./routes');
+const express = require("express");
+const cors = require("cors");
+const { errors } = require("celebrate");
+const routes = require("./routes");
 
 const app = express();
 
 app.use(cors()); //registra endereços válidos de requisições
 app.use(express.json()); //informa que o corpo das requisições devem ser parseados para JSON
-app.use(routes);
+app.use(routes); //inclui as rotas da nossa aplicação
+app.use(errors());
 
 app.listen(3333);
